c0adbb941df4c0919d49e5b5a6f6baa7869a279b
Christopher Theus <chritzy@hotmail.de>
2023-07-06 11:13:24 +0200

    add buttons interactivity

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// phew… not a lot going on here. Please add some code!
+// Get the bookmark buttons
+const bookmarkButtons = document.querySelectorAll(".bookmark");
+
+// Add click event listener to each bookmark button
+bookmarkButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    button.classList.toggle("bookmark--active");
+  });
+});
+
+// Get all answer buttons
+const answerButtons = document.querySelectorAll(".card__button-answer");
+
+// Add click event listener to each answer button
+answerButtons.forEach((button) => {
+  button.addEventListener("click", toggleAnswer);
+});
+
+function toggleAnswer() {
+  const answer = this.nextElementSibling;
+  answer.classList.toggle("hidden");
+  this.textContent = answer.classList.contains("hidden")
+    ? "Show answer"
+    : "Hide answer";
+}
